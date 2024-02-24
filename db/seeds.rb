978b1d58215e942010c00203fe4111e9d04bdbb6
diopus <googopus@gmail.com>
2024-02-24 15:01:50 +0100

    added entities with relations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[ "Клавиши", "Двери", "Лужи" ].each do |category_name|
+  Category.find_or_create_by!(name: category_name)
+end
