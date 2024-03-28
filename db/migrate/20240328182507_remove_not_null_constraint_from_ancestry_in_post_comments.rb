83547d0b1001ffa923145e280ef3729a22bab172
diopus <googopus@gmail.com>
2024-03-28 19:26:47 +0100

    removed NotNULLConstraint from "ancestry" field in post_comments

---
+++ db/migrate/20240328182507_remove_not_null_constraint_from_ancestry_in_post_comments.rb
@@ -0,0 +1,5 @@
+class RemoveNotNullConstraintFromAncestryInPostComments < ActiveRecord::Migration[7.1]
+  def change
+    change_column_null :post_comments, :ancestry, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_27_185221) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_28_182507) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_27_185221) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
-    t.string "ancestry", null: false
+    t.string "ancestry"
     t.index ["ancestry"], name: "index_post_comments_on_ancestry"
     t.index ["post_id"], name: "index_post_comments_on_post_id"
     t.index ["user_id"], name: "index_post_comments_on_user_id"
